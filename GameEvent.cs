e48d7a75e0e8b45f51893defa82e4138d6c76e7a
ErikOverflow <46797334+ErikOverflow@users.noreply.github.com>
2019-02-23 19:13:46 -0600

    Add Scriptable Events

---
+++ GameEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "New Game Event", order = 1)]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        for(int i = listeners.Count -1; i >= 0; i--)
+        {
+            listeners[i].OnEventsRaised();
+        }
+    }
+
+    public void Raise(GameObject go)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventsRaised(go);
+            listeners[i].OnEventsRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        listeners.Remove(listener);
+    }
+}
---
+++ GameEventListener.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent Event;
+    public bool ContainsGameObject;
+    public UnityEvent Response;
+    public EventGameObject ResponseWithObject;
+
+    private void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventsRaised()
+    {
+        Response.Invoke();
+    }
+
+    public void OnEventsRaised(GameObject go)
+    {
+        ResponseWithObject.Invoke(go);
+    }
+}
+
+[Serializable]
+public class EventGameObject : UnityEvent<GameObject> { }
+
+[CustomEditor(typeof(GameEventListener))]
+[CanEditMultipleObjects]
+public class GameEventListenerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        GameEventListener gel = target as GameEventListener;
+        EditorGUILayout.BeginHorizontal();
+        gel.ContainsGameObject = EditorGUILayout.Toggle("Does this Event contain a GameObject?", gel.ContainsGameObject);
+        EditorGUILayout.EndHorizontal();
+
+        gel.Event = (GameEvent) EditorGUILayout.ObjectField("Event", gel.Event, typeof(GameEvent), true);
+
+        if (gel.ContainsGameObject)
+        {
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("ResponseWithObject"));
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("Response"));
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+}
