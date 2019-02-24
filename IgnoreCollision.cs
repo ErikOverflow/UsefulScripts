a6c57b107d69e937f3b63149f749144cc5279b77
ErikOverflow <46797334+ErikOverflow@users.noreply.github.com>
2019-02-23 21:04:27 -0600

    Add IgnoreCollision script

---
+++ IgnoreCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D ignoredCollider;
+    [SerializeField]
+    private Collider2D myCollider;
+
+
+    private void Start()
+    {
+        Physics2D.IgnoreCollision(myCollider, ignoredCollider);
+    }
+}
